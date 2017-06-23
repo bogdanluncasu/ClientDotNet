dec614b2f2fd5e5d536bd24401aa99ab9a0f9327
BogdanLuncasu <luncasub11@gmail.com>
2017-06-23 12:04:21 +0300

    catch rangeerror

--- wwwroot/js/recogniser.js
+++ wwwroot/js/recogniser.js
@@ -10,66 +10,6 @@ var readfile = function () {
 		array = new Uint8Array(arrayBuffer),
 		binaryString = String.fromCharCode.apply(null, array);
         binaryString = btoa(binaryString)
-        $.ajax({
-            url: "https://objectrecognize.herokuapp.com/image/issport",
-            type: "POST",
-            data: JSON.stringify({ "image": binaryString }),
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            success: function (data) {
-                imagesource = $("#imagesource");
-                labelinfo = $("#labelinfo");
-                labelinfo.html(data["label"].toUpperCase());
-                if (data["label"] == "sport") {
-                    labelinfo.css('color', 'green');
-                    
-                    $.ajax({
-                        url: "https://objectrecognize.herokuapp.com/image/objectrecognition",
-                        type: "POST",
-                        data: JSON.stringify({ "image": binaryString }),
-                        contentType: "application/json; charset=utf-8",
-                        dataType: "json",
-                        success: function (data) {
-                            image=data["image"]
-                            imagesource.attr('src', 'data:image/jpeg;base64,' + btoa(image));
-                            objectdiv = $("#objects");
-
-                            objectdiv.html("");
-                            element = $('<ul></ul>');
-                            objects = data["objects"]
-                            objects.sort(function(a, b) { 
-                                return b.matches - a.matches;
-                            })
-
-                            var maxmatches = 1;
-                            if (objects.length > 0)
-                                maxmatches = objects[0]['matches']
-
-                            for (var obj in objects) {
-                                objname = objects[obj]['object'];
-                                color = objects[obj]['color'];
-                                matches = objects[obj]['matches'];
-                                color = color[2] + ',' + color[1] + ',' + color[0];
-
-                                element.append($('<li></li>')
-                                    .attr({style:'color:rgb('+color+')' })
-                                    .text(objname+' - '+parseInt(((matches/maxmatches)*100)))
-                                );
-                            }
-                            objectdiv.append(element);
-                            console.log(data["objects"]);
-                        }
-                    })
-
-                } else {
-                    labelinfo.css('color', 'red');
-                    imagesource.attr('src', 'data:image/jpeg;base64,' + binaryString);
-                }
-                console.log(data["label"]);
-            }
-        });
-
-
 
     }
     reader.readAsArrayBuffer(image[0].files[0]);
@@ -80,9 +20,6 @@ var readfile = function () {
 $("#recognise").click(function (e) {
 	
     console.log("Recognise pressed");
-	try{
-		readfile();
-	}catch(err){
-		alert("File size is too big.")
-	}
+	readfile();
+
 });
